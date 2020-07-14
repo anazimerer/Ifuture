fe96fbf6dd6df918f81c6a01ba6f7e618e73ad60
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-13 23:45:24 -0300

    atalhos na splashscreen

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { findByLabelText } from "@testing-library/react";
 
 const SplashScreen = () => {
-  return <div>Splash</div>;
+  const history = useHistory();
+  const handleClick = (e) => {
+    history.push(e.target.value);
+  };
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <button value={"/login"} onClick={handleClick}>
+        login
+      </button>
+      <button value={"/signup"} onClick={handleClick}>
+        signup
+      </button>
+      <button value={"/address"} onClick={handleClick}>
+        address
+      </button>
+      <button value={"/restaurants"} onClick={handleClick}>
+        restaurants
+      </button>
+      <button value={"/search"} onClick={handleClick}>
+        search
+      </button>
+      <button value={"/cart"} onClick={handleClick}>
+        cart
+      </button>
+      <button value={"/orders"} onClick={handleClick}>
+        orders
+      </button>
+      <button value={"/profile"} onClick={handleClick}>
+        profile
+      </button>
+    </div>
+  );
 };
 
 export default SplashScreen;

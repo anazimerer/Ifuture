c46fff44fcf266e7b1d09fef5606c37262497d51
edupezz <edupezz@gmail.com>
2020-07-14 15:30:45 -0300

    FeedPage, primeira manipulação, a terminar

---
+++ src/components/FeedPage/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const TitleBarContainer = styled.div `
+
+    width: 100vw;
+    height: 4rem;
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+`
+export const Title = styled.div `
+
+    width: 10.938rem;
+    height: 2.75rem;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    text-align: center;
+    margin-top: 1.25rem;
+`

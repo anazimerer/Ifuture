3c56ca812fb13ec0e845262df4df30bd223405c2
yuzokamoto <yuzokamoto@gmail.com>
2020-07-14 15:10:06 -0300

    SplashScreenPage finalizada

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { Container, Grow } from '@material-ui/core';
+
+import logoIcon from '../../img/logo.svg';
 
 const SplashScreen = () => {
-  return <div>Splash</div>;
+  const history = useHistory();
+
+  useEffect(() => {
+    setTimeout(() => {
+      history.replace('/login');
+    }, 3000);
+  }, [history]);
+
+  return (
+    <Container
+      style={{
+        height: '100vh',
+        padding: 0,
+        backgroundColor: '#e8222e',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+      maxWidth='md'
+    >
+      <Grow in={true} timeout={1500}>
+        <img src={logoIcon} alt='ifuture' />
+      </Grow>
+    </Container>
+  );
 };
 
 export default SplashScreen;

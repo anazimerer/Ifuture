71428a2ea98753f23fe85982697f50974376fbb4
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-14 01:15:12 -0300

    uma funcao de teste na splashscreen

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { findByLabelText } from "@testing-library/react";
 
 const SplashScreen = () => {
   const history = useHistory();

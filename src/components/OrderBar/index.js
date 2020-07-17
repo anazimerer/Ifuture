6b11e5ece4f144e190f0af4c39c0d0a6141ba382
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-16 22:30:46 -0300

    OrderBar

---
+++ src/components/OrderBar/index.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+import { useLocation } from "react-router-dom";
+
+import { getActiveOrder } from "../../functions/axios";
+
+import {
+  OrderAppBar,
+  Wrapper,
+  Clock,
+  Restaurant,
+  Label,
+  Total,
+} from "./styles";
+
+const OrderBar = (props) => {
+  const { haveFooter } = props;
+  const [activeOrder, setActiveOrder] = useState();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getActiveOrder();
+      setActiveOrder(response);
+    })();
+  }, []);
+
+  const clock = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+    >
+      <path
+        fill="#FFF"
+        fillRule="evenodd"
+        d="M17 9h-2v8.525L22 22l1-1.748-6-3.793V9zm-1 18C9.922 27 5 22.078 5 16S9.922 5 16 5s11 4.922 11 11-4.922 11-11 11zm-.014-25C8.258 2 2 8.272 2 16s6.258 14 13.986 14C23.728 30 30 23.728 30 16S23.728 2 15.986 2z"
+      />
+    </svg>
+  );
+
+  return activeOrder ? (
+    <OrderAppBar havefooter={haveFooter ? 1 : 0} open={activeOrder}>
+      <Wrapper>
+        <Clock>{clock}</Clock>
+        <Label>Pedido em andamento</Label>
+        <Restaurant>{activeOrder.restaurantName}</Restaurant>
+        <Total>SUBTOTAL R${activeOrder.totalPrice.toFixed(2)}</Total>
+      </Wrapper>
+    </OrderAppBar>
+  ) : null;
+};
+
+export default OrderBar;
---
+++ src/components/OrderBar/styles.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+
+import AppBar from "@material-ui/core/AppBar";
+
+export const OrderAppBar = styled(AppBar)`
+  height: 7.375rem;
+  background-color: #e8222e;
+  backdrop-filter: blur(10px);
+  top: auto;
+  bottom: ${({ havefooter }) => (havefooter ? "3rem" : "0")};
+  backdrop-filter: blur(10px);
+  box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
+  position: fixed;
+  display: ${({ open }) => (open ? "inherit" : "none")};
+`;
+
+export const Wrapper = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 5rem 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-areas:
+    "clock label"
+    "clock restaurant"
+    "clock total";
+`;
+
+export const Clock = styled.div`
+  grid-area: clock;
+  margin-left: 1.5rem;
+  margin-top: 2.75rem;
+`;
+
+export const Label = styled.div`
+  grid-area: label;
+  font-family: roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: #ffffff;
+  margin-top: 1.5rem;
+`;
+
+export const Restaurant = styled.div`
+  grid-area: restaurant;
+  font-family: Montserrat;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: black;
+  margin-top: 0.5rem;
+`;
+
+export const Total = styled.div`
+  grid-area: total;
+  font-family: Montserrat;
+  font-size: 1rem;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  color: black;
+`;

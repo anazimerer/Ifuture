58db0069b7f6fa48c885e7b75868113352ae1441
Renan Takeshi <rtakeshi13@gmail.com>
2020-07-16 22:51:26 -0300

    limpando

--- src/components/OrderBar/index.js
+++ src/components/OrderBar/index.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 
-import { useLocation } from "react-router-dom";
-
 import { getActiveOrder } from "../../functions/axios";
 
 import {
@@ -16,8 +14,6 @@ import {
 const OrderBar = (props) => {
   const { haveFooter } = props;
   const [activeOrder, setActiveOrder] = useState();
-
-  const location = useLocation();
 
   useEffect(() => {
     (async () => {
